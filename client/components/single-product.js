52a191f14f361a4f014b99dd55bbc73e994dd67d
beccasouza <rebeccacsouza@gmail.com>
2021-03-02 16:51:22 -0600

    added single product view, reducer and route

---
+++ client/components/single-product.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchSingleProduct} from '../store/single-product'
+
+class SingleProduct extends Component {
+  componentDidMount() {
+    this.props.getSingleProduct(this.props.match.params.productId)
+  }
+
+  render() {
+    const product = this.props.product
+    return (
+      <div>
+        <div>
+          <h2>{product.product_name}</h2>
+          <img src={product.product_image} />
+          <p>{product.product_price}</p>
+          <p>{product.product_description}</p>
+        </div>
+        <div />
+      </div>
+    )
+  }
+}
+
+const mapState = state => {
+  return {product: state.singleProduct}
+}
+
+const mapDispatch = dispatch => {
+  return {
+    getSingleProduct: id => dispatch(fetchSingleProduct(id))
+  }
+}
+
+export default connect(mapState, mapDispatch)(SingleProduct)
