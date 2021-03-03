79675f707a06e3c9d97e33863f280f116bc1c01c
ginareeena <ginamoffit@gmail.com>
2021-03-03 11:44:30 -0500

    added two seed tests

--- tests/tier-01/seed.spec.js
+++ tests/tier-01/seed.spec.js
@@ -2,7 +2,21 @@
 /* global describe it */
 
 const seed = require('../../script/seed')
+const {User, Product} = require('../server/db/models')
 
 describe('seed script', () => {
   xit('completes successfully', seed)
+})
+
+describe('Seed file', () => {
+  beforeEach(seed)
+
+  xit('populates the database with at least two users', async () => {
+    const seededUsers = await User.findAll()
+    expect(seededUsers).to.have.lengthOf.at.least(2)
+  })
+  xit('it populates the database with at least 5 products', async () => {
+    const seededProducts = await Product.findAll()
+    expect(seededProducts).to.have.lengthOf.at.least(5)
+  })
 })
