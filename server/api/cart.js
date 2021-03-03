edff17dd94156a421e4cd1e95a737769d8946443
ginareeena <ginamoffit@gmail.com>
2021-03-03 17:50:43 -0500

    updated cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,14 +2,14 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+// guest cart
+
+// currently cart is set up
+
+// logged in user cart
 router.get('/user/:id', async (req, res, next) => {
   try {
-    const user = await User.findByPk({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
+    const user = await User.findByPk(req.params.id)
     res.json(user.user_cart)
   } catch (err) {
     next(err)
