66b6c66abc64aac53ac22284a07a5a9e3ac7fb33
ginareeena <ginamoffit@gmail.com>
2021-03-03 11:51:17 -0500

    seed tests passing

--- tests/tier-01/seed.spec.js
+++ tests/tier-01/seed.spec.js
@@ -1,21 +1,21 @@
 'use strict'
-/* global describe it */
 
+// Make sure to create meowmazon-test in your database so these run!
+
+const {expect} = require('chai')
 const seed = require('../../script/seed')
-const {User, Product} = require('../server/db/models')
 
-describe('seed script', () => {
-  xit('completes successfully', seed)
-})
+const {User, Product} = require('../../server/db/models')
 
 describe('Seed file', () => {
   beforeEach(seed)
 
-  xit('populates the database with at least two users', async () => {
+  it('populates the database with at least two users', async () => {
     const seededUsers = await User.findAll()
     expect(seededUsers).to.have.lengthOf.at.least(2)
   })
-  xit('it populates the database with at least 5 products', async () => {
+
+  it('it populates the database with at least 5 products', async () => {
     const seededProducts = await Product.findAll()
     expect(seededProducts).to.have.lengthOf.at.least(5)
   })
