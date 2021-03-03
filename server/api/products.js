820cf1ec6dcfd9aca47e0f98e5c1dde7a9e800d4
Lindsay Welhoelter <lwelhoelter@gmail.com>
2021-03-03 14:37:34 -0500

    added all products view

--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,17 @@ const router = require('express').Router()
 const {Product} = require('../db/models')
 module.exports = router
 
+//GET /api/products
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    console.log(products)
+    res.json(products)
+  } catch (error) {
+    next(error)
+  }
+})
+
 //GET /api/products/:productId
 router.get('/:productId', async (req, res, next) => {
   try {
