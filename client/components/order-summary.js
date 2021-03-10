181cc6be86a9f2ef06d52ff458d6010a4208b021
Lindsay Welhoelter <lwelhoelter@gmail.com>
2021-03-09 23:15:40 -0500

    checkout form styling

---
+++ client/components/order-summary.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {submitOrder, getOrder} from '../store/orders'
+
+class OrderSummary extends Component {
+  componentDidMount() {
+    const user = this.props.user
+    if (user.id) {
+      this.props.fetchOrder(user.id)
+    }
+    // if (user.user_billing_address) {
+    //   this.setState({
+    //     shippingAddress: user.user_shipping_address,
+    //     billingAddress: user.user_billing_address,
+    //   })
+    // }
+  }
+  render() {
+    let order = this.props.order
+    return (
+      <div className="OrderSummary">
+        <div className="Title">Order Summary</div>
+        <table>
+          <tbody>
+            <tr>
+              <td>Initial Total</td>
+              <td>${order.order_price / 100}</td>
+            </tr>
+            <tr>
+              <td>Discount</td>
+              <td>$0</td>
+            </tr>
+            <tr>
+              <td>Subtotal</td>
+              <td>${order.order_price / 100}</td>
+            </tr>
+            <tr>
+              <td>Tax</td>
+              <td>$0</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="Total">
+          <div className="TotalLabel">Total</div>
+          <div className="Amount">
+            total <small>${order.order_price / 100}</small>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapState = state => {
+  return {user: state.user, order: state.order}
+}
+
+const mapDispatch = dispatch => {
+  return {
+    fetchOrder: userId => dispatch(getOrder(userId)),
+    submitOrder: order => dispatch(submitOrder(order))
+  }
+}
+
+export default connect(mapState, mapDispatch)(OrderSummary)
