8a278d0b9ada0df8aea66aeee3bc3c0c37cd1566
Lindsay Welhoelter <lwelhoelter@gmail.com>
2021-03-09 20:25:28 -0500

    added footer

---
+++ client/components/footer.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+import React, {Component} from 'react'
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-middle">
+      <div className="container">
+        <div className="row">
+          <div className="footer-column">
+            <h4>Contact Us</h4>
+            <h5>care@meowmazon.com</h5>
+            <h5>123.456.789</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="footer-middle">
+      <div className="container">
+        <div className="row">
+          <div className="footer-column">
+            <h4>Team Members</h4>
+            <h5>Gina Moffit</h5>
+            <h5>Becca Souza</h5>
+            <h5>Lindsay Welhoelter</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
